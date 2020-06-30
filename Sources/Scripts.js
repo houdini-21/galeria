401ec64d410c803c621242e1028e0e6ffec6c7a4
Fernando Jose Marinero Cruz <marinerocruzfer@gmail.com>
2020-06-30 14:31:33 -0600

    maquetacion lista, trabajando con js

---
+++ Sources/Scripts.js
@@ -0,0 +1,25 @@
+const blur = document.getElementById("blur");
+const sepia = document.getElementById("sepia");
+const saturacion = document.getElementById("saturacion");
+const negativo = document.getElementById("negativo");
+const grises = document.getElementById("grises");
+
+blur.addEventListener("click", function (e) {
+  alert("efecto blur");
+});
+
+sepia.addEventListener("click", function (e) {
+  alert("efecto blur");
+});
+
+saturacion.addEventListener("click", function (e) {
+  alert("efecto blur");
+});
+
+negativo.addEventListener("click", function (e) {
+  alert("efecto blur");
+});
+
+grises.addEventListener("click", function (e) {
+  alert("efecto blur");
+});
