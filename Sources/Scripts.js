ee3976097035ce11cee9e2624e59ad840f8b461d
Fernando Jose Marinero Cruz <marinerocruzfer@gmail.com>
2020-07-03 10:32:55 -0600

    funcionando todo, aun faltan correcciones en codigo para optimizacion y agregar animaciones con css

--- Sources/Scripts.js
+++ Sources/Scripts.js
@@ -5,9 +5,10 @@ const btnEstilos = document.querySelectorAll(".btn-style");
 const btnCategorias = document.querySelectorAll(".btn-categorias");
 const boxImgCategorias = document.querySelectorAll(".box-img-categorias");
 let imagenOld = "montanas1";
-let style = "";
 let btnOldId = "montanas-btn";
+let categoriaBoxOldId = "montanas";
 let styledImage = false;
+let btnStyle, style;
 
 btnEstilos.forEach(function (h) {
   h.addEventListener("click", agregarEstilosaImagen);
@@ -24,41 +25,50 @@ btnCategorias.forEach(function (u, index) {
   });
 });
 
-function cambiarCategoria(i) {
-  const btnNewId = i.id;
+function cambiarCategoria(d) {
+  const btnNewId = d.id;
   const btnNew = document.getElementById(btnNewId).classList;
   const btnOld = document.getElementById(btnOldId).classList;
   if (btnOld !== btnNewId) {
     btnOld.remove("btn-active");
     btnNew.add("btn-active");
-    console.log(btnNewId, "remplazara a", btnOld);
     btnOldId = btnNewId;
-    console.log(btnOld, "remplazo a", btnNewId);
   }
 }
 
-function ocultarCategoria(d, m) {
-  const categoriaBox = d[m].classList;
-  categoriaBox.remove("hidden");
+function ocultarCategoria(i, n) {
+  const categoriaBoxNewId = i[n].id;
+  const categoriaBoxNew = document.getElementById(categoriaBoxNewId).classList;
+  const categoriaBoxOld = document.getElementById(categoriaBoxOldId).classList;
+
+  if (categoriaBoxOld !== categoriaBoxNewId) {
+    categoriaBoxOld.add("hidden");
+    categoriaBoxNew.remove("hidden");
+    categoriaBoxOldId = categoriaBoxNewId;
+  }
 }
 
-function agregarEstilosaImagen(o) {
-  style = o.srcElement.id;
-  const btnStyle = o.srcElement.classList;
+function agregarEstilosaImagen(c) {
+  style = c.srcElement.id;
+  btnStyle = c.srcElement.classList;
   btnStyle.toggle("style-active");
   estilos.classList.toggle(style);
   styledImage = true;
 }
 
-function mostrarImagenSeleccionada(d) {
-  const image = d.srcElement.id;
+function mostrarImagenSeleccionada(b) {
+  const image = b.srcElement.id;
   imagen.classList.replace(imagenOld, image);
   imagenOld = image;
   if (styledImage == true) {
     estilos.classList.remove(style);
+    btnStyle.remove("style-active");
   }
 }
 
 /**  document.getElementById('btn-playa').addEventListener('click', function(e){
   document.getElementById('playa').style.display = 'block';
+  document.getElementById('montana').style.display = 'none';
+  document.getElementById('ciudad').style.display = 'none';
+  document.getElementById('paisaje').style.display = 'none';
 })*/
