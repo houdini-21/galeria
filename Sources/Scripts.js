ee48752e5f50896f7414a107bc28d49ea804a60c
Fernando Jose Marinero Cruz <marinerocruzfer@gmail.com>
2020-07-09 09:38:27 -0600

    Buenas practicas agregadas, cambio en favicon, todo listo para la revision uwu

--- Sources/Scripts.js
+++ Sources/Scripts.js
@@ -1,26 +1,25 @@
-let estilos = document.getElementById("estilos");
-const imagenes = document.querySelectorAll(".imagenes-categorias");
+let containerStyles = document.getElementById("containerStyles");
+const categorias = document.querySelectorAll(".imagenes-categorias");
 const imagen = document.getElementById("imagen");
 const btnEstilos = document.querySelectorAll(".btn-style");
 const btnCategorias = document.querySelectorAll(".btn-categorias");
 const boxImgCategorias = document.querySelectorAll(".box-img-categorias");
-let imagenOld = "montanas1";
+let imagenOldId = "montanas1";
 let btnOldId = "montanas-btn";
 let categoriaBoxOldId = "montanas";
-let styledImage = false;
-let btnStyle, style;
+let btnStyleClass, style, stylizedImg;
 
 btnEstilos.forEach((h) => {
   h.addEventListener("click", agregarEstilosaImagen);
 });
 
-imagenes.forEach((o) => {
+categorias.forEach((o) => {
   o.addEventListener("click", mostrarImagenSeleccionada);
 });
 
-btnCategorias.forEach((u, index) => {
+btnCategorias.forEach((u, z) => {
   u.addEventListener("click", () => {
-    ocultarCategoria(boxImgCategorias, index);
+    ocultaryMostrarCategoria(boxImgCategorias, z);
     cambiarColorBtnCategoria(u);
   });
 });
@@ -36,34 +35,38 @@ cambiarColorBtnCategoria = (d) => {
   }
 };
 
-function ocultarCategoria (i, n) {
+function ocultaryMostrarCategoria(i, n) {
   const categoriaBoxNewId = i[n].id;
   const categoriaBoxNew = document.getElementById(categoriaBoxNewId).classList;
   const categoriaBoxOld = document.getElementById(categoriaBoxOldId).classList;
 
   if (categoriaBoxOldId !== categoriaBoxNewId) {
-    categoriaBoxOld.add("hidden");
-    categoriaBoxOld.remove("fadeIn");
-    categoriaBoxNew.remove("hidden");
-    categoriaBoxNew.add("fadeIn");
+    categoriaBoxOld.replace("fadeIn", "hidden");
+    categoriaBoxNew.replace("hidden", "fadeIn");
     categoriaBoxOldId = categoriaBoxNewId;
   }
-};
+}
 
-function agregarEstilosaImagen (c) {
-  style = c.srcElement.id;
-  btnStyle = c.srcElement.classList;
-  btnStyle.toggle("style-active");
-  estilos.classList.toggle(style);
-  styledImage = true;
-};
+function agregarEstilosaImagen(c) {
+  styleId = c.srcElement.id;
+  btnStyleClass = c.srcElement.classList;
+  btnStyleClass.toggle("style-active");
+  containerStyles.classList.toggle(styleId);
+  stylizedImg = true;
+}
 
 function mostrarImagenSeleccionada(b) {
-  const image = b.srcElement.id;
-  imagen.classList.replace(imagenOld, image);
-  imagenOld = image;
-  if (styledImage == true) {
-    estilos.classList.remove(style);
-    btnStyle.remove("style-active");
+  const imgSelectedId = b.srcElement.id;
+  const imageSelectedNew = document.getElementById(imgSelectedId).classList;
+  const imageSelectedOld = document.getElementById(imagenOldId).classList;
+  if (imgSelectedId !== imagenOldId) {
+    imageSelectedNew.add("img-selected");
+    imageSelectedOld.remove("img-selected");
+  }
+  imagen.classList.replace(imagenOldId, imgSelectedId);
+  imagenOldId = imgSelectedId;
+  if (stylizedImg) {
+    containerStyles.classList.remove(styleId);
+    btnStyleClass.remove("style-active");
   }
 }
